d14be2133438f543496fa875d92501f3127e6b84
Olga Gaydash <olga.gaydash@rakuten.com>
2021-05-10 21:42:56 +0300

    hw11

---
+++ src/com/gaydash/ListPerson.java
@@ -0,0 +1,16 @@
+package com.gaydash;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListPerson {
+
+    private List<Person> listPerson = new ArrayList<>();
+
+    public void addHuman(Person human) {
+        listPerson.add(human);
+    }
+
+
+
+}
---
+++ src/com/gaydash/Person.java
@@ -0,0 +1,74 @@
+package com.gaydash;
+// Создать класс Person который описывает человека, с полями: имя, фамилия, возраст, вес, рост.
+
+public class Person {
+    private String firstName;
+    private String lastName;
+    private int age;
+    private double weight;
+    private double height;
+
+    public Person(String firstName, String lastName, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    public Person(String firstName, String lastName, double weight) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.weight = weight;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", age=" + age +
+                ", weight=" + weight +
+                ", height=" + height +
+                '}';
+    }
+}
+
